7db8250b58a5ed639e8fba5f3574d639c886affe
Jerry4013 <jerryzhang4013@gmail.com>
2022-07-01 14:12:58 -0400

    update

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t jerry4013/multi-client:latest -t jerry4013/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jerry4013/multi-server:latest -t jerry4013/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jerry4013/multi-worker:latest -t jerry4013/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker puth jerry4013/multi-client:latest
+docker puth jerry4013/multi-server:latest
+docker puth jerry4013/multi-worker:latest
+
+docker puth jerry4013/multi-client:$SHA
+docker puth jerry4013/multi-server:$SHA
+docker puth jerry4013/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=jerry4013/multi-server:$SHA
+kubectl set image deployments/client-deployment client=jerry4013/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=jerry4013/multi-worker:$SHA
